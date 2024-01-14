dac849b7e9958f34faea761eb40afb90f7b3ee35
Nicolas Hopkins <seekerking123@gmail.com>
2024-01-13 18:54:46 -0700

    I finished this program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        
+        //find a random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        //display the "pre" guess
+        int Myguess = -100;
+
+        
+
+        //use if statements to find out if the guess is correct
+
+        while (Myguess != number)
+        {   Console.WriteLine("Pick a number between 1 and 100");
+            Myguess = int.Parse(Console.ReadLine());
+            if (Myguess > number)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else if (Myguess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine($"You got it! the number was {number}");   
+            }
+        }
     }
 }
